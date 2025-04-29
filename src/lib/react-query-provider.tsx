0af7304242eed51d079793779b6b485b6cbea1ad
PauloDaude <paulodaude14@gmail.com>
2025-04-29 10:38:35 -0300

    chore: create initial enviroment

---
+++ src/lib/react-query-provider.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode, useState } from "react";
+
+export function ReactQueryClientProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60 * 5,
+            retry: 2,
+            refetchOnWindowFocus: false,
+          },
+          mutations: {
+            retry: false,
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
