77c2c3f6ba20ad54598b99d03436d6c606ab7571
PauloDaude <paulodaude14@gmail.com>
2025-05-13 16:32:07 -0300

    feat: add one more error code

--- src/utils/error-util.ts
+++ src/utils/error-util.ts
@@ -11,15 +11,14 @@ export const errorCodes = {
   NOT_FOUND_REQUEST_ERROR: "NotFoundRequestError",
   TOKEN_EXPIRED: "TokenExpired",
   CONFLICT_REQUEST_ERROR: "ConflictRequestError",
-  UNKNOWN_ERROR: "UnknownError",
   GOOGLE_LOGIN_REQUEST_ERROR: "GoogleLoginRequestError",
+  USER_DISABLED_ERROR: "UserDisabledError",
 } as const;
 
 export type ErrorCode = (typeof errorCodes)[keyof typeof errorCodes];
 
 export const getFriendlyMessage = (error: ApiError): string => {
   const [code, ...messageParts] = error.message.split(":");
-  const message = messageParts.join(":").trim();
 
   const messages: Record<string, string> = {
     [errorCodes.INVALID_CREDENTIALS_ERROR]:
@@ -30,10 +29,11 @@ export const getFriendlyMessage = (error: ApiError): string => {
     [errorCodes.CONFLICT_REQUEST_ERROR]: "Email já cadastrado, faça o login",
     [errorCodes.GOOGLE_LOGIN_REQUEST_ERROR]:
       "Usuário cadastrado via Google, use o login com Google",
-    [errorCodes.UNKNOWN_ERROR]: "Ocorreu um erro inesperado",
+    [errorCodes.USER_DISABLED_ERROR]:
+      "Usuário está desativado, entre em contato com o suporte",
   };
 
-  return messages[code] || message || messages[errorCodes.UNKNOWN_ERROR];
+  return messages[code];
 };
 
 export const formatAndThrowError = (
