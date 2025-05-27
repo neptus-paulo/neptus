6332ae027436f6fbee578207b0ad28d6def355fd
PauloDaude <paulodaude14@gmail.com>
2025-05-27 20:16:33 -0300

    fix: adjust paddings in side bar menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,4 @@
-import {
-  Bolt,
-  ChartPie,
-  History,
-  LayoutDashboard,
-  MenuIcon,
-  Waves,
-} from "lucide-react";
-import Image from "next/image";
+import { Bolt, ChartPie, History, MenuIcon, Waves } from "lucide-react";
 import Link from "next/link";
 
 import AppButton from "./AppButton";
@@ -32,41 +24,33 @@ const Header = () => {
             <SheetTitle className="py-4 text-xl">Menu</SheetTitle>
           </SheetHeader>
           <div className="flex flex-col gap-2">
-            <Link href="/" className="flex items-center gap-2 w-full">
-              <AppButton
-                variant="ghost"
-                className="w-full justify-start rounded-none"
-              >
-                <ChartPie className="w-10" />
-                <span className="text-lg">Dashboard</span>
-              </AppButton>
+            <Link
+              href="/"
+              className="flex items-center gap-2 w-full hover:bg-accent py-2 px-4"
+            >
+              <ChartPie />
+              <span className="text-lg">Dashboard</span>
             </Link>
-            <Link href="/" className="flex items-center gap-2 w-full">
-              <AppButton
-                variant="ghost"
-                className="w-full justify-start py-6 rounded-none"
-              >
-                <History />
-                <span className="text-lg">Histórico</span>
-              </AppButton>
+            <Link
+              href="/"
+              className="flex items-center gap-2 w-full hover:bg-accent py-2 px-4"
+            >
+              <History />
+              <span className="text-lg">Histórico</span>
             </Link>
-            <Link href="/" className="flex items-center gap-2 w-full">
-              <AppButton
-                variant="ghost"
-                className="w-full justify-start py-6 rounded-none"
-              >
-                <Bolt />
-                <span className="text-lg">Configurações</span>
-              </AppButton>
+            <Link
+              href="/"
+              className="flex items-center gap-2 w-full hover:bg-accent py-2 px-4"
+            >
+              <Bolt />
+              <span className="text-lg">Configurações</span>
             </Link>
-            <Link href="/" className="flex items-center gap-2 w-full">
-              <AppButton
-                variant="ghost"
-                className="w-full justify-start py-6 rounded-none"
-              >
-                <Waves />
-                <span className="text-lg">Tanques</span>
-              </AppButton>
+            <Link
+              href="/"
+              className="flex items-center gap-2 w-full hover:bg-accent py-2 px-4"
+            >
+              <Waves />
+              <span className="text-lg">Tanques</span>
             </Link>
           </div>
         </SheetContent>
