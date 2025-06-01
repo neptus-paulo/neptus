1884b360909c891cfdaa41c691222953cdd1ce19
PauloDaude <paulodaude14@gmail.com>
2025-06-01 16:36:00 -0300

    refactor: add custom variant styles

--- src/components/AppButton.tsx
+++ src/components/AppButton.tsx
@@ -6,17 +6,37 @@ import { signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
+interface AppButtonProps extends React.ComponentProps<typeof Button> {
+  isLoading?: boolean;
+}
+
 const AppButton = ({
   isLoading,
+  className,
+  variant,
   ...props
-}: React.ComponentProps<typeof Button> & { isLoading?: boolean }) => {
+}: AppButtonProps) => {
+  const variantModifications = {
+    default: "",
+    destructive: "",
+    outline: "shadow-none border-foreground/15 focus:border-foreground/30",
+    secondary: "",
+    ghost: "",
+    link: "",
+  };
+
   return (
     <Button
       {...props}
+      variant={variant}
       disabled={isLoading || props.disabled}
-      className={cn("rounded-md cursor-pointer", props.className)}
+      className={cn(
+        "cursor-pointer transition-all duration-200",
+        variant && variantModifications[variant],
+        className,
+      )}
     >
-      {isLoading && <Loader2 className="animate-spin" />}
+      {isLoading && <Loader2 className="animate-spin mr-2 h-4 w-4" />}
       {props.children}
     </Button>
   );
